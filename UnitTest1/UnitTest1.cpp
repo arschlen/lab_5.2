fb0227a84cf0adf8b038c2f9012d9766a9957a46
arschlen <arsnchld08@gmail.com>
2024-10-08 00:19:36 +0300

    arschlen

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,25 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include <cmath>
+#include "../lab_5.2/5.2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+    TEST_CLASS(UnitTest1)
+    {
+    public:
+
+        TEST_METHOD(TestMethod1)
+        {
+            double eps = 1;
+            double s = 1;
+            int n = 1;
+
+            S(2.0, eps, n, s);
+
+            Assert::IsTrue(fabs(log(2.0) - s) < eps, L"Test failed for x = 2.0");
+        }
+    };
+}
